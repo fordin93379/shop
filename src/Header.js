b00aabb69c4e3f7806f9378cca298fcd7e117dcd
Snowdy 35 <asganich@gmail.com>
2022-04-05 21:26:44 +0300

    Feg

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,8 @@ function Header() {
   return (
     <div className="Header">
       <h2 className='ContainerToCenter'>Купи толчок!</h2>
-
+      <br></br>
+       <h3 className='sall'></h3>
     </div>
   );
 }
