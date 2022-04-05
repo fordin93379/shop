dc9eace3b4c21ad6661e2c89b01ce64270182ab9
Snowdy 35 <asganich@gmail.com>
2022-04-05 21:38:22 +0300

    jimhig

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,9 @@ import './Header.css';
 function Header() {
   return (
     <div className="Header">
-
+      <h2 className='tolchok'>Купи толчёк!</h2>
+     <br></br>
+     <h3 className='sell'>Только в течении этой недели скидки на всё до -983235%</h3>
     </div>
   );
 }
