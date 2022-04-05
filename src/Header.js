a7b7e27ff2c897e5644a48796d2fa3b4856239ba
Fedor <fordin2020@gmail.com>
2022-04-05 16:42:19 +0300

    !

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ import './Header.css';
 function Header() {
   return (
     <div className="Header">
-      <h2>Купи талчок!Если Федя пидор</h2>
+      <h2 className='ContainerToCenter'>Купи талчок!Если Федя пидор</h2>
 
     </div>
   );
