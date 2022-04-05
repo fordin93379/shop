ca5a7b9b498962becb9b28a473d199b2559a3f44
Crystal <rustam.bezz@gmail.com>
2022-04-05 16:35:40 +0300

    !

--- src/Main.js
+++ src/Main.js
@@ -2,7 +2,8 @@ import './Main.css'
 function Main() {
   return (
     <div className="Main">
-
+     <h1 className="title">О нас </h1>
+     <p>Мы продаём унитазы уже 24 года.Наши унитазы очень качественные и уже проверены.</p>
     </div>
   );
 }
