d035ebcfa3e46f7b922d33dc77af6ac84d875672
Snowdy 35 <asganich@gmail.com>
2022-04-05 21:47:26 +0300

    n

--- src/Header.js
+++ src/Header.js
@@ -5,6 +5,8 @@ function Header() {
       <h2 className='tolchok'>Купи толчёк!</h2>
      <br></br>
      <h3 className='sell'>Только в течении этой недели скидки на всё до -983235%</h3>
+     <br></br>
+     <h1></h1>
     </div>
   );
 }
