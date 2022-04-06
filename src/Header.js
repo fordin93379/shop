260b98cd804e672ef2fd8297daa8115a1158728a
Snowdy 35 <asganich@gmail.com>
2022-04-06 20:17:04 +0300

    k

--- src/Header.js
+++ src/Header.js
@@ -7,10 +7,11 @@ function Header() {
       <h2 className='tolchok'>Купи толчёк!</h2>
      <br></br>
      <h3 className='sell'>Только в течении этой недели скидки на всё до -983235%</h3>
-     <img src={dik} width="300" height="250"/>
+     <img src={dik} width="720" height="250"/>
      <br></br>
      <h1 className='Fedy'>Федя ПИДОР</h1>
     <img src={pic} width="300" height="250"/>
+    
     </div>
   );
 }
